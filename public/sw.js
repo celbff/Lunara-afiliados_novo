f76be1d80ad1397ca8abf71f5ba8453020e5971a
Cel | Lunara <celsot.holistica@gmail.com>
2025-08-19 18:23:51 -0300

    Atualizar o sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,46 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('lunara').then(cache => cache.addAll(['/'])));
+// public/sw.js
+const CACHE_NAME = 'lunara-afiliados-v1';
+const urlsToCache = [
+  '/',
+  '/pages/DashboardPage',
+  '/pages/AgendaPage',
+  '/pages/PacientesPage',
+  '/pages/RelatoriosPage',
+  '/pages/TerapiasPage',
+  '/imagens/logo.webp',
+  '/css/style.css',
+  '/js/script.js'
+];
+
+// Instalação: cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// Ativação: limpeza de caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: serve do cache ou rede
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
